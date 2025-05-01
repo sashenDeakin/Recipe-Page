b0676e3989cd67222d9e317d0102d152be2fc0ba
SashenJayathilaka <sashenhasindu@gmail.com>
2025-05-01 21:04:39 +1000

    Created  My recipes Page

--- src/components/data/data.js
+++ src/components/data/data.js
@@ -3,15 +3,13 @@ export const recipeData = [
     mins: "10",
     title: "Creamy Salad",
     serving: 4,
-    image:
-      "https://s3-alpha-sig.figma.com/img/8a7e/4773/6bd295e18dc61af191c1442b30a8ab2e?Expires=1745798400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=LXH~R~nHYahwu3Yf5YEzDxlskjQ-RatfjGi5p1Vf-Nw~58Ct6DunsmoGdIdaPHB8wJGE7BRYWL4AiO5oGCxKC5nR1lGBdcMw7mYBh~v03lfpddvYeGupOt2mSd3Shx-1mpa02h-8-GaYXyTa5ETZvuO2zXE8vB7fk3BOVTE0j70AetSi086Z1avgI5fllAAfgKgiQ37oWFHjnurHD8ov9-e6DdvWj7I9~U9Eex1t3Z26mffW2OyT8GBmCbbmHaUJmudVK8v2Kgu545RamZLL7OgifLpEKxNK0BOjnt47ufia-pp11EYQG2ouTwA6DYc5Z~5BL1vH-rR0kZbAj3Mq5g__",
+    image: "",
   },
   {
     mins: "15",
     title: "Tofu Tomatoes Soup ",
     serving: 4,
-    image:
-      "https://s3-alpha-sig.figma.com/img/29e9/b9ca/ac514c2dd216d74a6da0a912f8c618fe?Expires=1745798400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=LShfZ~w1D1y0M96pwilw1kjh9NMt57p9feIskkSfN-nU-TL827cMmFZjrWcjHqsThwjyQSjCGG0soqmcPMQ4p~BHxpS8OoEHBxIf-qYp7d6jS-522iFTHzmcMxghRKDs~tIp1~2OJMdDOqUiRLdcF7W8y9FbuRfLYBjbRMRNshzgnRkuAejhCvnkovZKYubCGjyuNme~S3CEVut-QiMLkNSm3Ei~9u0WkCFG7vbW3CbbGn73H7wfT~vppktsKEqh8lAvNtHio5hHXdb9LWemLBJEUdP-tyH6EbacjwHlx0AitjKlq7kQw9dAgxGApA58y72FgPTDKAV45Aa0KXDFKg__",
+    image: "",
   },
   {
     mins: "10",
